40b8c101c5d0936081e78df9f0632dfc92775f3e
CoolsElliot <cools.elliot@gmail.com>
2022-02-15 10:58:56 +0100

    navbar

--- Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/Nav/Nav.js
+++ Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/Nav/Nav.js
@@ -3,11 +3,40 @@ import React from "react"
 class Nav extends React.Component {
     render(){
         return(
-            <div className="col-lg-12">
+            <div className="col-lg-12" id="nav">
+                <div className="col-lg-3 fs-10">
+                    <h1>Fashe <span className="fs-12 text-danger">.</span></h1>
+                </div>
                 
+                <nav className="d-flex col-lg-6">
+                    <ul className="d-flex justify-content-around">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Products</a>
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="col-lg-3">
+                    <div className="d-flex">
+                        <div className="">
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Nav
+export default Nav 
