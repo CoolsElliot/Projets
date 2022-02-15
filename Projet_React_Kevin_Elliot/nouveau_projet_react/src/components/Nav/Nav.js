c69ed33791c0b669908d3965eca2daefc8db2366
CoolsElliot <cools.elliot@gmail.com>
2022-02-15 11:37:03 +0100

    projetreact

--- Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/Nav/Nav.js
+++ Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/Nav/Nav.js
@@ -1,36 +1,40 @@
 import React from "react"
 
 class Nav extends React.Component {
-    render(){
-        return(
-            <div className="col-lg-12" id="nav">
-                <div className="col-lg-3 fs-10">
-                    <h1>Fashe <span className="fs-12 text-danger">.</span></h1>
-                </div>
-                
-                <nav className="d-flex col-lg-6">
-                    <ul className="d-flex justify-content-around">
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">Products</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                        <li>
-                            <a href="">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="col-lg-3">
-                    <div className="d-flex">
-                        <div className="">
-                            <i class="fa-solid fa-heart"></i>
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 col-xl-12 d-flex align-items-center justify-content-around" id="nav">
+                        <div className="col-3 col-lg-3 col-xl-3 d-flex align-item-center m-0 fs-6">
+                            <h1>Fashe <span>.</span></h1>
                         </div>
-                        <div>
-                            <i class="fa-solid fa-basket-shopping"></i>
+
+                        <nav className="d-flex m-0 col-6 col-lg-6 col-xl-6">
+                            <ul className="d-flex list-unstyled" id="menulist">
+                                <li className="">
+                                    <a href="">Home</a>
+                                </li>
+                                <li className="">
+                                    <a href="">Products</a>
+                                </li>
+                                <li className="">
+                                    <a href="">About</a>
+                                </li>
+                                <li className="">
+                                    <a href="">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className="col-3 col-lg-3 col-xl-3 m-0">
+                            <div className="d-flex">
+                                <div className="d-flex align-items-center">
+                                    <i class="fa-solid fa-heart fs-4"></i>
+                                </div>
+                                <div className="d-flex align-items-center">
+                                    <i class="fa-solid fa-basket-shopping fs-4 "></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/footer/Footer.js
+++ Projet_React_Kevin_Elliot/nouveau_projet_react/src/components/footer/Footer.js
@@ -1,10 +1,30 @@
 import React from "react"
 
 class Footer extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="col-lg-12">
-                
+                <div className="col-lg-6">
+                    <h3>GET IN TOUCH</h3>
+                    <p>Any questions? Let us know in store at 8th floor, 379 Hudson St, New York,</p>
+                    <p>NY 10018 or call</p>
+                </div>
+                <div className="col-lg-6">
+                    <div className="col-lg-6">
+                        <h3>LINKS</h3>
+                        <a href="">Search</a>
+                        <a href="">About Us</a>
+                        <a href="">Contact Us</a>
+                        <a href="">Returns</a>
+                    </div>
+                    <div className="col-lg-6">
+                        <h3>LINKS</h3>
+                        <a href="">Search</a>
+                        <a href="">About Us</a>
+                        <a href="">Contact Us</a>
+                        <a href="">Returns</a>
+                    </div>
+                </div>
             </div>
         );
     }
